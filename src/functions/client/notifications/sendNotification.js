c481487c454bdf4958cc6004520083b8fdce8964
Agustin Marcano <agustinzirit@gmail.com>
2022-05-03 22:15:43 -0400

    logs

--- src/functions/client/notifications/sendNotification.js
+++ src/functions/client/notifications/sendNotification.js
@@ -3,25 +3,31 @@ const { SNS } = require("aws-sdk");
 
 /* A function that is being exported from the file. */
 module.exports.notification = async (clientID) => {
-  /* Creating a variable called params that is an object with two properties. The first property is
-  called Message and it is an object with a property called clientID. The second property is called
-  TopicArn and it is a string. */
-  var params = {
+
+  /* Creación de un nuevo objeto SNS y publicación de un mensaje en el tema. */
+  const params = {
     Message: JSON.stringify({ clientID: clientID }),
     TopicArn:
     "arn:aws:sns:us-east-1:661329848707:aws-academy-dev-clientCreatedSNS",
   };
-  /* Creación de un nuevo objeto de SNS y publicación del objeto params en el tema de SNS. */
   const sns = new SNS().publish(params).promise();
 
-  // Handle promise's fulfilled/rejected states
-  /* A promise that is going to publish the params object to the SNS topic. */
+  console.log("Visualizar log");
+  console.log(JSON.stringify({params}));
+
+  /* Una promesa que se está resolviendo. */
   sns
     .then(function (data) {
-      console.log({data});
+      console.log("Se publico correctamente...");
+      console.log(
+        `Message ${params.Message} sent to the topic ${params.TopicArn}`
+      );
       console.log("MessageID is " + data.MessageId);
+
+      console.log("Fin de la publicacion...");
     })
     .catch(function (err) {
-      console.error(err, err.stack);
+      console.log("Hubo un error... ");
+      console.log(err, err.stack);
     });
 };
