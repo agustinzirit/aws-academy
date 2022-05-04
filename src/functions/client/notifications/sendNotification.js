1f708f5e653d1d4d4843bd1e35c87b27e91c97c2
Agustin Marcano <agustinzirit@gmail.com>
2022-05-04 15:32:11 -0400

    sns updated

--- src/functions/client/notifications/sendNotification.js
+++ src/functions/client/notifications/sendNotification.js
@@ -1,7 +1,8 @@
 /* Importing the SNS object from the aws-sdk. */
 const { SNS } = require("aws-sdk");
 /* Importando la función `sendSQSCard` desde el archivo `assignCard.js` ubicado en la carpeta `queue`. */
-const sendSQSCard = require("../../../queue/assignCard");
+// const sendSQSCard = require("../../../queue/assignCard");
+const sns = new SNS();
 
 /* A function that is being exported from the file. */
 module.exports.notification = async (clientID) => {
@@ -9,14 +10,14 @@ module.exports.notification = async (clientID) => {
   /* Creación de un nuevo objeto SNS y publicación de un mensaje en el tema. */
   const params = {
     Message: clientID,
-    TopicArn:
-    "arn:aws:sns:us-east-1:661329848707:aws-academy-dev-clientCreatedSNS",
+    TopicArn:	process.env.CLIENT_CREATED_TOPIC_ARN
   };
-  const sns = new SNS()//.publish(params).promise();
 
+	console.log(params.TopicArn);
   console.log("Visualizar log");
   console.log("test");
 
+	/* Publicación de un mensaje al tema. */
 	sns.publish(params, (err, data) => {
 		if (err) {
 			console.log("Hubo un error... ");
@@ -29,7 +30,7 @@ module.exports.notification = async (clientID) => {
 			console.log("Fin de la publicacion...");
 
 			/* Envío del ID de cliente a la cola de Card. */
-			sendSQSCard(params.Message);
+			// sendSQSCard(params.Message);
 		}
 	});
 };
