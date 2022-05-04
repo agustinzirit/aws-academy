fed9952cabfad7d924b34174fa493f4326252e48
Agustin Marcano <agustinzirit@gmail.com>
2022-05-04 13:38:32 -0400

    sns modified

--- src/functions/client/notifications/sendNotification.js
+++ src/functions/client/notifications/sendNotification.js
@@ -12,28 +12,24 @@ module.exports.notification = async (clientID) => {
     TopicArn:
     "arn:aws:sns:us-east-1:661329848707:aws-academy-dev-clientCreatedSNS",
   };
-  const sns = new SNS().publish(params).promise();
+  const sns = new SNS()//.publish(params).promise();
 
   console.log("Visualizar log");
   console.log("test");
 
-  /* Una promesa que se está resolviendo. */
-  sns
-    .then(function (data) {
-      console.log("Se publico correctamente...");
-      console.log(
-        `Message ${params.Message} sent to the topic ${params.TopicArn}`
-      );
-      console.log("MessageID is " + data.MessageId);
+	sns.publish(params, (err, data) => {
+		if (err) {
+			console.log("Hubo un error... ");
+			console.log(err, err.stack);
+		} else {
+			console.log("Se publico correctamente...");
+			console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+			console.log("MessageID is " + data.MessageId);
 
-      console.log("Fin de la publicacion...");
+			console.log("Fin de la publicacion...");
 
 			/* Envío del ID de cliente a la cola de Card. */
 			sendSQSCard(params.Message);
-
-    })
-    .catch(function (err) {
-      console.log("Hubo un error... ");
-      console.log(err, err.stack);
-    });
+		}
+	});
 };
