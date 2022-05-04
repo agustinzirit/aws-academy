d09a2588931b7193726a9213d75df3be9b4b3085
Agustin Marcano <agustinzirit@gmail.com>
2022-05-03 21:50:57 -0400

    fixed sns publish

--- src/functions/client/notifications/sendNotification.js
+++ src/functions/client/notifications/sendNotification.js
@@ -3,29 +3,20 @@ const { SNS } = require("aws-sdk");
 
 /* A function that is being exported from the file. */
 module.exports.notification = async (clientID) => {
-  /* Creating a new SNS object. */
-  const sns = new SNS();
-  /* Logging the clientID to the console. */
-  console.log(clientID);
-  // Create publish parameters
   /* Creating a variable called params that is an object with two properties. The first property is
   called Message and it is an object with a property called clientID. The second property is called
   TopicArn and it is a string. */
   var params = {
     Message: JSON.stringify({ clientID: clientID }),
     TopicArn:
-      "arn:aws:sns:us-east-1:661329848707:aws-academy-dev-clientCreatedSNS",
+    "arn:aws:sns:us-east-1:661329848707:aws-academy-dev-clientCreatedSNS",
   };
-
-  // Create promise and SNS service object
-  /* Creating a promise that is going to publish the params object to the SNS topic. */
-  var publishTextPromise = sns
-    .publish(params)
-    .promise();
+  /* Creación de un nuevo objeto de SNS y publicación del objeto params en el tema de SNS. */
+  const sns = new SNS().publish(params).promise();
 
   // Handle promise's fulfilled/rejected states
   /* A promise that is going to publish the params object to the SNS topic. */
-  publishTextPromise
+  sns
     .then(function (data) {
       console.log(JSON.stringify(data));
       // console.log(
